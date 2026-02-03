21faa6570f7acb6471cfccd73b9b6f32ddfa305c
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-03 19:29:50 +0200

    Add initial App.vue layout and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
+  <nav class="d-flex gap-2 justify-content-center">
+    <router-link to="/">Home</router-link>
+    <router-link v-if="isLoggedIn" to="/locations/new">Lisa kiisu</router-link>
+
+    <button v-if="isLoggedIn" type="button" class="btn btn-secondary btn-sm ms-3">
+      Logi välja
+    </button>
+
+    <button v-else type="button" class="btn btn-secondary btn-sm ms-3">
+      Logi sisse / registreeri
+    </button>
+  </nav>
   <router-view />
 </template>
-
-<script setup>
-
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      isLoggedIn: false,
+      isAdmin: false,
+    }
+  }
+}
 </script>
-
