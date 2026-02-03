e7128983fe1cbe258785615d0c91c245f1a19734
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-03 21:00:04 +0200

    App.vue justify-content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="d-flex gap-2 justify-content-center">
+  <nav class="d-flex gap-2 justify-content-start m-5">
     <router-link to="/">Home</router-link>
-    <router-link v-if="isLoggedIn" to="/locations/new">Lisa kiisu</router-link>
+    <router-link v-if="isLoggedIn">Lisa kiisu</router-link>
 
     <button v-if="isLoggedIn" type="button" class="btn btn-secondary btn-sm ms-3">
       Logi välja
