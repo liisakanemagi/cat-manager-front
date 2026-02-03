10d19d3eaa3b6740fa9134334c153a667de64723
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-03 20:50:13 +0200

    Add initial Homeview layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
-<script setup>
+<template>
+  <div class="home">
+    <img class="full-screen-image" src="@/assets/illustrations/cat1.jpeg" alt="Cat illustration">
+  </div>
+</template>
 
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  }
+}
 </script>
 
-<template>
-
-</template>
+<style scoped>
+.full-screen-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  z-index: -1;
+}
+</style>
