0aaddce6914739af803c3ea834dc7b57bd723f4d
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-06 15:47:10 +0200

    RegisterView.vue first template

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,42 @@
 <template>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col-12 col-sm-8 col-md-6 col-lg-4">
+        <div class="d-grid gap-2 mt-3">
 
+          <div class="home">
+            <img class="login-cat" src="@/assets/illustrations/cat3.jpeg" alt="Cat illustration">
+          </div>
+
+          <div class="form-floating">
+            <input type="text" class="form-control" placeholder="Kasutajanimi">
+            <label>Kasutajanimi</label>
+          </div>
+
+          <div class="form-floating">
+            <input type="text" class="form-control" placeholder="Email">
+            <label>Email</label>
+          </div>
+
+          <div class="form-floating">
+            <input type="password" class="form-control" placeholder="Parool">
+            <label>Parool</label>
+          </div>
+
+          <div class="form-floating">
+            <input type="password" class="form-control" placeholder="Korda parooli">
+            <label>Korda parooli</label>
+          </div>
+
+          <button class="btn btn-secondary" :disabled="isPostingData">
+            <span class="spinner-border spinner-border-sm me-2" aria-hidden="true"></span>
+            Registreeri
+          </button>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +50,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.login-cat {
+  max-width: 300px;
+  width: 100%;
+  height: auto;
+}
+</style>
