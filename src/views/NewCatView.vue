6f1756e9d838505f308a426fa9a683d6608d7649
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-16 14:59:22 +0200

    3 columns arranged for NewCatView.vue template

--- src/views/NewCatView.vue
+++ src/views/NewCatView.vue
@@ -1,15 +1,78 @@
 <template>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col-12">
 
+        <AlertError
+            :alert-error-message='alertErrorMessage'
+            @event-alert-box-closed='resetAlertMessages'
+        />
 
+        <AlertSuccess
+            :alert-success-message='alertSuccessMessage'
+            @event-alert-box-closed='resetAlertMessages'
+        />
+      </div>
+    </div>
+
+    <form v-if="displayAllFields" @submit.prevent novalidate>
+      <div class="row">
+        <div class="col-12 col-md-4 d-flex align-items-center justify-content-center mb-3">
+          <div class="home">
+            <img
+              class="login-cat img-fluid"
+              src="@/assets/illustrations/cat4.jpeg"
+              alt="Cat illustration"
+            >
+          </div>
+        </div>
+
+        <div class="col-12 col-md-2 d-flex flex-column gap-2">
+          <div class="form-floating">
+            <input
+                v-model="cat.name"
+                type="text"
+                class="form-control"
+                placeholder="Kassinimi"
+                required
+                maxlength="50"
+            >
+            <label>Kassi nimi</label>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4 d-flex flex-column gap-2">
+          <div class="d-grid gap-2 mt-3">
+            <button
+                type="submit"
+                class="btn btn-secondary"
+                :disabled="isPostingData"
+            >
+              <span
+                  v-if="isPostingData"
+                  class="spinner-border spinner-border-sm me-2"
+                  aria-hidden="true"
+              ></span>
+              Lisa
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import AlertError from "@/components/AlertError.vue";
+import AlertSuccess from "@/components/AlertSuccess.vue";
+
 export default {
   name: 'NewCatView',
+  components: {AlertSuccess, AlertError},
   data() {
     return {
 
-      userId: Number(sessionStorage.getItem(usedId)),
+      userId: Number(sessionStorage.getItem('usedId')),
       isPostingData: false,
       displayAllFields: true,
       alertErrorMessage: '',
@@ -43,7 +106,14 @@ export default {
 
     }
   },
-  methods: {},
+  methods: {
+
+    resetAlertMessages() {
+      this.alertErrorMessage = ''
+      this.alertSuccessMessage = ''
+    },
+
+  },
   mounted() {
   }
 }
