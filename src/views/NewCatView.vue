25d8c17157b629a5d2a1920699693242a8e8771b
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-16 13:54:40 +0200

    NewCatView.vue initial data block created

--- src/views/NewCatView.vue
+++ src/views/NewCatView.vue
@@ -1,14 +1,47 @@
 <template>
 
 
-
 </template>
 
 <script>
 export default {
   name: 'NewCatView',
   data() {
-    return {}
+    return {
+
+      userId: Number(sessionStorage.getItem(usedId)),
+      isPostingData: false,
+      displayAllFields: true,
+      alertErrorMessage: '',
+      alertSuccessMessage: '',
+
+      cat: {
+        name: '',
+        statusId: 0,
+        arrivedAt: '',
+        birthday: '',
+        weight: 0,
+        sex: '',
+        chipNumber: '',
+        healthInfo: '',
+        otherInfo: '',
+        imageData: '',
+      },
+
+      catStatuses:
+          [
+            {
+              id: '',
+              label: ''
+            }
+          ],
+
+      errorResponse: {
+        message: '',
+        errorCode: 0
+      },
+
+    }
   },
   methods: {},
   mounted() {
