db78dd98f57fb8c5039a39d55bba792afe9b7b1e
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-23 15:47:37 +0200

    feat - sendGetCatsRequest added to CatService.js

--- src/services/CatService.js
+++ src/services/CatService.js
@@ -2,12 +2,23 @@ import axios from "axios";
 
 export default {
 
-    sendPostCatRequest(cat){
+    sendPostCatRequest(cat) {
         let token = sessionStorage.getItem('token');
         return axios.post('/cat', cat, {
             headers: {
                 Authorization: 'Bearer ' + token
             }
         })
+    },
+
+    sendGetCatsRequest() {
+        let token = sessionStorage.getItem('token');
+        return axios.get('/cats',
+        {
+            headers:{
+                Authorization: 'Bearer ' + token
+            }
+        })
+
     }
 }
