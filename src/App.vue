845cd18b539bba992007c404440f70663caef896
liisakanemagi <liisa.kanemagi@gmail.com>
2026-02-24 14:51:51 +0200

    feat: fetch cats list and add CatDropDown to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <nav class="d-flex gap-2 justify-content-center">
     <router-link to="/">Avaleht</router-link>
-    <router-link to="/cat/new" v-if="isLoggedIn">Lisa kiisu</router-link>
+
+    <CatDropDown
+        v-if="isLoggedIn"
+        :cats="cats"
+        :selected-cat-id="cats.id"
+        @event-new-cat-selected="setNewCatId"
+    />
+
+    <router-link to="/cat/new" v-if="isLoggedIn">Lisa kass</router-link>
 
     <button v-if="isLoggedIn" @click="logOut" type="button" class="btn btn-secondary btn-sm ms-3">
       Logi välja
@@ -16,14 +24,34 @@
 <script>
 import navigationService from "@/services/NavigationService";
 import SessionStorageService from "@/services/SessionStorageService";
+import CatDropDown from "@/components/Cat/CatDropDown.vue";
+import catService from "@/services/CatService";
 
 export default {
   name: 'App',
+  components: {CatDropDown},
 
   data() {
     return {
       isLoggedIn: false,
       isAdmin: false,
+
+      cats: [
+        {
+          id: 0,
+          name: '',
+          statusId: 0,
+          sex: '',
+          arrivedAt: '',
+          birthday: '',
+          weight: 0,
+          chipNumber: '',
+          healthInfo: '',
+          otherInfo: '',
+          imageData: '',
+        }
+      ],
+
     }
   },
 
@@ -43,10 +71,24 @@ export default {
     navigateToLoginView() {
       navigationService.navigateToLoginView()
     },
+
+    async getCats() {
+      try {
+        const response = await catService.sendGetCatsRequest()
+        this.cats = response.data
+      } catch (error) {
+        navigationService.navigateToErrorView()
+      }
+    },
+
+    setNewCatId(selectedCatId) {
+      this.cats.id = selectedCatId
+    },
   },
 
   mounted() {
     this.updateNavMenu()
+    this.getCats()
   }
 }
 </script>
