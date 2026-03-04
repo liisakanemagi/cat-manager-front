9122f5ca1fb6248a23d5170136e40dc4d4c28bf1
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-04 18:57:10 +0200

    feat: create CatView with inital data & getCat method

---
+++ src/views/CatView.vue
@@ -0,0 +1,50 @@
+<template>
+
+</template>
+
+<script>
+import catService from "@/services/CatService";
+import navigationService from "@/services/NavigationService";
+
+export default {
+  name: 'CatView',
+  data() {
+    return {
+
+      catId: Number(this.$route.params.id),
+
+      cat:
+          {
+            id: 0,
+            name: '',
+            statusId: 0,
+            arrivedAt: '',
+            birthday: '',
+            weight: 0,
+            sex: '',
+            chipNumber: '',
+            healthInfo: '',
+            otherInfo: '',
+            imageData: '',
+          }
+
+    }
+  },
+  methods: {
+
+    async getCat() {
+
+      try {
+        const response = await catService.sendGetCatRequest(this.catId)
+        this.cat = response.data
+      } catch (error) {
+        navigationService.navigateToErrorView()
+      }
+    }
+  },
+
+  mounted() {
+    this.getCat()
+  }
+}
+</script>
